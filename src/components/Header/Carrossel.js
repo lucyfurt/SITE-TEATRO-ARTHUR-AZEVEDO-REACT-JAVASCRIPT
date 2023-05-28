7d838b18612f9d134449cc777463139591d69470
lucyfurt <lucianafurtado10@gmail.com>
2023-05-28 11:08:43 -0300

    carrossel responsivo

--- src/components/Header/Carrossel.js
+++ src/components/Header/Carrossel.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import { Carousel } from 'antd';
 
-
 function Carrossel() {
   return (
     <Carousel autoplay>
-      <div >
-
-        <img src='https://i.ibb.co/w0CdjFv/taa-banner-2.png' />
-
+      <div>
+        <img
+          src='https://i.ibb.co/w0CdjFv/taa-banner-2.png'
+          style={{ width: '100%', height: 'auto' }}
+        />
       </div>
-      <div  >
-
-        <img src='https://i.ibb.co/w0CdjFv/taa-banner-2.png' />
-
+      <div>
+        <img
+          src='https://i.ibb.co/w0CdjFv/taa-banner-2.png'
+          style={{ width: '100%', height: 'auto' }}
+        />
       </div>
-
     </Carousel>
-  )
-
+  );
 }
+
 export default Carrossel;
-
-
-
-
