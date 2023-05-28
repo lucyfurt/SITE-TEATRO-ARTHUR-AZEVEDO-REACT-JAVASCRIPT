f823082080c5cb2af6fb7c606a2c403b0b35c4cc
lucyfurt <lucianafurtado10@gmail.com>
2023-05-28 13:13:21 -0300

    fix cardnews

--- src/components/Content/CardNews.js
+++ src/components/Content/CardNews.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Row, Col, Space } from 'antd';
+import { Row, Col } from 'antd';
 import { FileTextOutlined } from '@ant-design/icons';
 import { Card } from 'antd';
 
@@ -50,17 +50,29 @@ function CardNews() {
               description="O Núcleo de Memória e Pesquisa do TAA possui acervo riquíssimo e crescente de obras e documentos históricos referentes às artes cênicas, além de projetos de pesquisa e extensão que são desenvolvidas em parceria com instituições públicas, como a UFMA."
             />
           </Card>
+        </Col> 
+        <Col xs={24} sm={12} lg={6}>
+          <Card
+            style={{
+              width: '100%',
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://i.ibb.co/y6BjDVk/Whats-App-Image-2023-05-25-at-17-40-32.jpg"
+              />
+            }
+            actions={[
+              <FileTextOutlined key="read2">Leia mais</FileTextOutlined>
+            ]}
+          >
+            <Meta
+              title="entreATO"
+              description="O TAA promove a segunda edição do entreATO, cujo objetivo é promover ações artístico-pedagógicas, nas quais os participantes possam experimentar as mais diversas metodologias de criação cênica."
+            />
+          </Card>
         </Col>
-        <Space
-          direction="vertical"
-          size="middle"
-          style={{
-            display: 'flex',
-          }}
-        />
-      </Row>
-      <Row gutter={[16, 16]}>
-        <Col xs={24} sm={12} lg={8}>
+        <Col xs={24} sm={12} lg={6}>
           <Card
             style={{
               width: '100%',
@@ -81,51 +93,8 @@ function CardNews() {
             />
           </Card>
         </Col>
-        <Col xs={24} sm={12} lg={8}>
-          <Card
-            style={{
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://i.ibb.co/y6BjDVk/Whats-App-Image-2023-05-25-at-17-40-32.jpg"
-              />
-            }
-            actions={[
-              <FileTextOutlined key="read4">Leia mais</FileTextOutlined>
-            ]}
-          >
-            <Meta
-              title="entreAto"
-              description="O entreAto é um ciclo de formação que explora processos criativos em contexto de grupo e conhecimentos técnicos de uma casa de espetáculos, nos quais eles possam se retroalimentar no decorrer de sua experimentação."
-            />
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={8}>
-          <Card
-            style={{
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://i.ibb.co/pJTrCbf/206taa.png"
-              />
-            }
-            actions={[
-              <FileTextOutlined key="read5"></FileTextOutlined>
-            ]}
-          >
-            <Meta
-              title="Logo significado"
-              description="A logo dos 206 anos transmite uma mensagem de movimento, conexão e união."
-            />
-          </Card>
-        </Col>
       </Row>
     </>
   );
 }
-
 export default CardNews;
