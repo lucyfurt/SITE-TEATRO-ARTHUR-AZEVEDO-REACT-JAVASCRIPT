89069793b3708536553e81fbb981c27fd66573ac
lucyfurt <lucianafurtado10@gmail.com>
2023-05-29 15:47:40 -0300

    carrosel updated

--- src/components/Header/Carrossel.js
+++ src/components/Header/Carrossel.js
@@ -2,18 +2,30 @@ import React from 'react';
 import { Carousel } from 'antd';
 
 function Carrossel() {
+  const isSmallScreen = window.innerWidth < 768; // Adjust the breakpoint as needed
+
   return (
     <Carousel autoplay>
+      {/* Slide 1 */}
       <div>
         <img
-          src='https://i.ibb.co/w0CdjFv/taa-banner-2.png'
-          style={{ width: '100%', height: 'auto' }}
+          src='https://i.ibb.co/0VFYkCT/cortina.png'
+          style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
         />
       </div>
+
+      {/* Slide 2 */}
       <div>
         <img
-          src='https://i.ibb.co/w0CdjFv/taa-banner-2.png'
-          style={{ width: '100%', height: 'auto' }}
+          src='https://i.ibb.co/xs1YHnM/lustre.png'
+          style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
+        />
+      </div>
+      {/* Slide 3 */}
+      <div>
+        <img
+          src='https://i.ibb.co/qkKQXzj/cabines.png'
+          style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
         />
       </div>
     </Carousel>
