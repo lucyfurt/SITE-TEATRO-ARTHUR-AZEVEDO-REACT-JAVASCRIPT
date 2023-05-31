35c35d93d9edddec4488c78cfb345c4cd0b486e6
lucyfurt <lucianafurtado10@gmail.com>
2023-05-31 15:50:11 -0300

    updated visitacao

--- src/components/Content/Visitacao.js
+++ src/components/Content/Visitacao.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Button, Modal } from 'antd';
-import { useState } from 'react';
 import {
   EnvironmentTwoTone,
   BankTwoTone,
   CheckCircleTwoTone,
   MessageTwoTone
 } from '@ant-design/icons';
+import './Visitacao.css'
 
 function Visitacao() {
   const [size, setSize] = useState('large');
@@ -17,34 +17,47 @@ function Visitacao() {
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '50px' }}>
         <Row gutter={[16, 16]}>
           <Col xs={24} sm={12} md={12} lg={12}>
-            <h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><EnvironmentTwoTone />Turística</h1>
-            <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><strong>Terça das 14h às 17h</strong></p>
-            <p style={{ display: 'flex', justifyContent: 'justify', alignItems: 'justify', padding: '20px' }}>Não é necessário agendamento,
-              basta chegar à bilheteria e receber as instruções. Dependendo da quantidade de visitantes, a visita poderá ser dividida
-              em grupos para melhor aproveitamento e segurança de todos.</p>
+            <h1><EnvironmentTwoTone />Turística</h1>
+            <p><strong>Terça das 14h às 17h</strong></p>
+            <p>
+              Não é necessário agendamento, basta chegar à bilheteria e receber as instruções.
+              Dependendo da quantidade de visitantes, a visita poderá ser dividida em grupos
+              para melhor aproveitamento e segurança de todos.
+            </p>
           </Col>
           <Col xs={24} sm={12} md={12} lg={12}>
-            <h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><BankTwoTone />Instituições</h1>
-            <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><strong>Terça das 14h às 17h</strong></p>
-            <p style={{ display: 'flex', justifyContent: 'justify', alignItems: 'center', padding: '20px' }}>Agendamento por meio de ofício
-              contendo as seguintes informações: dia, quantidade de visitantes e faixa etária, deverá ser enviado para o email:</p>
-            <p style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}><strong><MessageTwoTone />pauta.taa@gmail.com</strong></p>
-            <p>.</p>
+            <h1><BankTwoTone />Instituições</h1>
+            <p><strong>Terça das 14h às 17h</strong></p>
+            <p>
+              Agendamento por meio de ofício contendo as seguintes informações: dia,
+              quantidade de visitantes e faixa etária, deverá ser enviado para o email:
+            </p>
+            <p><strong><MessageTwoTone />pauta.taa@gmail.com</strong></p>
           </Col>
         </Row>
       </div>
 
-      <Button style={{ backgroundColor: '#f2ae30',  }} className="button text-center" type="primary" shape="primary" size={size} onClick={() => setModal2Open(true)}>
+      <Button
+        style={{ backgroundColor: '#f2ae30' }}
+        className="button text-center"
+        type="primary"
+        shape="primary"
+        size={size}
+        onClick={() => setModal2Open(true)}
+      >
         Mais informações
       </Button>
+
       <Modal
         title="Protocolos"
         centered
         visible={modal2Open}
         onOk={() => setModal2Open(false)}
         onCancel={() => setModal2Open(false)}
-        style={{ backgroundColor: '#ffcc80',  }}
-        className="button text-center" type="primary" shape="primary"
+        style={{ backgroundColor: '#ffcc80' }}
+        className="button text-center"
+        type="primary"
+        shape="primary"
       >
         <p><CheckCircleTwoTone />Proibido fotos em câmera profissional sem uma autorização prévia.</p>
         <p><CheckCircleTwoTone />É permanentemente proibido entrada nas dependências internas do Teatro com alimentos e bebidas.</p>
