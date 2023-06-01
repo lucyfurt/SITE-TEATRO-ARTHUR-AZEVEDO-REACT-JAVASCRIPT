e9e14d75bdae42ab70c68dd161e412978f1b3c29
lucyfurt <lucianafurtado10@gmail.com>
2023-06-01 14:05:38 -0300

    footer updated

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -15,30 +15,31 @@ const FooterComponent = () => {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <Row justify="center" gutter={[8, 8]}>
+        <Row justify="center" gutter={[6, 6]}>
           <Col xs={24} sm={12} md={6}>
             <img src="https://i.ibb.co/vwybPfW/Asset-4-2x.png" alt="Logo" className="logo" />
           </Col>
           <Col xs={24} sm={12} md={6}>
             <img src="https://i.ibb.co/pQyyTs6/Asset-1-2x.png" alt="SECMA" className="secma-logo" />
           </Col>
-          <Col xs={24} md={12}>
-            <div className={getSocialLinksClassName()}>
-              <a href="https://www.instagram.com/teatroarthurazevedooficial/" target="_blank" rel="noreferrer">
-                <InstagramOutlined style={{ fontSize: '30px' }} />
-              </a>
-              <a href="https://www.facebook.com/teatroarthurazevedooficial/?locale=pt_BR" target="_blank" rel="noreferrer">
-                <FacebookOutlined style={{ fontSize: '30px' }} />
-              </a>
-              <a href="https://www.youtube.com/channel/UCDq1f20ufxLFjUdz5eqSEtg" target="_blank" rel="noreferrer">
-                <YoutubeOutlined style={{ fontSize: '30px' }} />
-              </a>
-            </div>
-          </Col>
+
         </Row>
-        <p className="copyright">
+        <br /><br />
+
+        <div className="copyright" style={{ margin: '10px' }}>
           Todos os direitos reservados - Teatro Arthur Azevedo {new Date().getFullYear()}
-        </p>
+        </div>
+        <div className={getSocialLinksClassName()}>
+          <a href="https://www.instagram.com/teatroarthurazevedooficial/" target="_blank" rel="noreferrer">
+            <InstagramOutlined style={{ fontSize: '30px' }} />
+          </a>
+          <a href="https://www.facebook.com/teatroarthurazevedooficial/?locale=pt_BR" target="_blank" rel="noreferrer">
+            <FacebookOutlined style={{ fontSize: '30px' }} />
+          </a>
+          <a href="https://www.youtube.com/channel/UCDq1f20ufxLFjUdz5eqSEtg" target="_blank" rel="noreferrer">
+            <YoutubeOutlined style={{ fontSize: '30px' }} />
+          </a>
+        </div>
         <BackTop />
       </div>
     </footer>
