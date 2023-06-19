77acd23e408645002c97e1247c6bd23f0a590ff3
lucyfurt <lucianafurtado10@gmail.com>
2023-06-19 14:30:24 -0300

    new cardNews

--- src/components/Content/CardNews.js
+++ src/components/Content/CardNews.js
@@ -1,113 +1,47 @@
-import React from 'react';
-import { Row, Col } from 'antd';
-import { FileTextOutlined } from '@ant-design/icons';
-import { Card } from 'antd';
-import './CardNews.css'
+import { Card, Row, Col } from 'antd';
 
 const { Meta } = Card;
 
-function CardNews() {
+const NewsComponent = () => {
+  const newsData = [
+    {
+      title: 'Notícia 1',
+      description: 'Descrição da notícia 1',
+      image: 'https://i.ibb.co/y6BjDVk/Whats-App-Image-2023-05-25-at-17-40-32.jpg',
+    },
+    {
+      title: 'Notícia 2',
+      description: 'Descrição da notícia 2',
+      image: 'https://i.ibb.co/pJTrCbf/206taa.png',
+    },
+    {
+      title: 'Notícia 3',
+      description: 'Descrição da notícia 3',
+      image: 'https://i.ibb.co/Zf2MNgH/Whats-App-Image-2023-05-25-at-17-40-23.jpg',
+    },
+    {
+      title: 'Notícia 4',
+      description: 'Descrição da notícia 4',
+      image: 'https://i.ibb.co/x7crZ5f/Brilho-Contraste-1.png',
+    },
+  ];
+
   return (
-    <>
-      <Row gutter={[16, 16]}>
-        <Col xs={24} sm={12} lg={6}>
-          <Card
-            style={{
-              flex: 1,
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="TAA 206 anos"
-                src="https://i.ibb.co/pJTrCbf/206taa.png"
-              />
-            }
-            actions={[
-              <FileTextOutlined key="read1">Leia mais</FileTextOutlined>
-            ]}
-          >
-            <Meta
-              title="TAA 206 anos"
-              description="O TAA está com novidade! Para comemorar os 206 anos de história, lançamos uma logo que transmite toda importância e valor desse evento.
-                           "/>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={6}>
-          <Card
-            style={{
-              flex: 1,
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://i.ibb.co/Zf2MNgH/Whats-App-Image-2023-05-25-at-17-40-23.jpg"
-              />
-            }
-            actions={[
-              <FileTextOutlined key="read2">Leia mais</FileTextOutlined>
-            ]}
-          >
-            <Meta
-              title="Núcleo de Memória e Pesquisa do TAA"
-              description="O Núcleo de Memória e Pesquisa do TAA possui acervo riquíssimo e crescente de obras e documentos históricos referentes às artes cênicas, além de projetos de pesquisa e extensão que são desenvolvidas em parceria com instituições públicas, como a UFMA."
-            />
-            <br />
-            <br />
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={6}>
-          <Card
-            style={{
-              flex: 1,
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="example"
-                src="https://i.ibb.co/y6BjDVk/Whats-App-Image-2023-05-25-at-17-40-32.jpg"
-              />
-            }
-            actions={[
-              <FileTextOutlined key="read2">Leia mais</FileTextOutlined>
-            ]}
-          >
-            <Meta
-              title="entreATO"
-              description="O TAA promove a segunda edição do entreATO, cujo objetivo é promover ações artístico-pedagógicas, nas quais os participantes possam experimentar as mais diversas metodologias de criação cênica."
-            />
-          </Card>
-        </Col>
-        <Col xs={24} sm={12} lg={6}>
-          <Card
-            style={{
-              flex: 1,
-              width: '100%',
-            }}
-            cover={
-              <img
-                alt="Logo significado"
-                src="https://i.ibb.co/y6BjDVk/Whats-App-Image-2023-05-25-at-17-40-32.jpg"
-              />
-            }
-            actions={[
-              <FileTextOutlined key="read3">Leia mais</FileTextOutlined>
-            ]}
-          >
-            <Meta
-              title="entreATO"
-              description="O TAA promove a segunda edição do entreATO, cujo objetivo é promover ações artístico-pedagógicas, nas quais os participantes possam experimentar as mais diversas metodologias de criação cênica."
-            />
-          </Card>
-        </Col>
+    <div style={{ padding: '20px' }}>
+      <Row gutter={[16, 16]} justify="center">
+        {newsData.map((news, index) => (
+          <Col xs={24} sm={12} md={8} lg={6} key={index}>
+            <Card
+              hoverable
+              cover={<img alt={news.title} src={news.image} />}
+            >
+              <Meta title={news.title} description={news.description} />
+            </Card>
+          </Col>
+        ))}
       </Row>
-    </>
+    </div>
   );
-}
-export default CardNews;
+};
+
+export default NewsComponent;
