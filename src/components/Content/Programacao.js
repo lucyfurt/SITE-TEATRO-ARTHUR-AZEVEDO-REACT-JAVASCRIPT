9cdd3ed617f58920aad98d4324948d94e5c34ce1
lucyfurt <lucianafurtado10@gmail.com>
2023-06-19 14:33:47 -0300

    programacao card

--- src/components/Content/Programacao.js
+++ src/components/Content/Programacao.js
@@ -48,6 +48,20 @@ const App = () => {
                         <Meta title="18.06" description="This is the description" />
                     </Card>
                 </Col>
+                <Col xs={24} sm={12} md={8} lg={6}>
+                    <Card
+                        style={{ width: '100%' }}
+                        cover={
+                            <img
+                                alt="example"
+                                src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+                            />
+                        }
+                        actions={[<ZoomInOutlined />]}
+                    >
+                        <Meta title="18.06" description="This is the description" />
+                    </Card>
+                </Col>
             </Row>
             <div style={{ marginTop: '50px', display: 'flex', justifyContent: 'flex-end' }}>
                 <span style={{ alignSelf: 'center', marginRight: '8px' }}>
