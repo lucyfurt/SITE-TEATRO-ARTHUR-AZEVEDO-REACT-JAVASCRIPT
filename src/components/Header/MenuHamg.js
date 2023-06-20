4c971f8bec1c00f418577c900b97ca59bf7a0ab7
lucyfurt <lucianafurtado10@gmail.com>
2023-06-20 18:02:09 -0300

    menu

--- src/components/Header/MenuHamg.js
+++ src/components/Header/MenuHamg.js
@@ -4,26 +4,23 @@ import './menuHamb.css';
 
 const MyMenu = () => {
   return (
-    <div className="menu-wrapper">
-      <h1>Menu</h1>
-      <Menu>
-        <a className="menu-item" href="/">
-          O Teatro
-        </a>
-        <a className="menu-item" href="/about">
-          Institucional
-        </a>
-        <a className="menu-item" href="/services">
-          Pautas
-        </a>
-        <a className="menu-item" href="/contact">
-          Informações Técnicas
-        </a>
-        <a className="menu-item" href="/contact">
+    <Menu>
+      <a className="menu-item" href="/">
+        O Teatro
+      </a>
+      <a className="menu-item" href="/about">
+        Institucional
+      </a>
+      <a className="menu-item" href="/services">
+        Pautas
+      </a>
+      <a className="menu-item" href="/contact">
+        Informações Técnicas
+      </a>
+      <a className="menu-item" href="/contact">
         Núcleo de Memória e Pesquisa
-        </a>
-      </Menu>
-    </div>
+      </a>
+    </Menu>
   );
 };
 
