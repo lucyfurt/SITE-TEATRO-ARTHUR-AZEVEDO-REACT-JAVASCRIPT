04c57af1a64e3b034d6cd38eb4068d4126872ef4
lucyfurt <lucianafurtado10@gmail.com>
2023-06-26 15:05:17 -0300

    form

--- src/Pages/Pautas.js
+++ src/Pages/Pautas.js
@@ -7,6 +7,19 @@ function ContactForm() {
   }
   return (
       <form onSubmit={handleSubmit}>
+        <label htmlFor="nome">
+        Nome: 
+      </label>
+      <input
+        id="nome"
+        type='text' 
+        name="nome"
+      />
+      <ValidationError 
+     
+        field="nome"
+        errors={state.errors}
+      />
       <label htmlFor="email">
         Email:
       </label>
@@ -26,20 +39,75 @@ function ContactForm() {
       <input
         id="data"
         type='date' 
-        name="data"
+        name="data inicio"
       />
       <ValidationError 
      
         field="email"
         errors={state.errors}
       />
-      <textarea
-        id="message"
-        name="message"
+      <label htmlFor="data">
+        Data fim:
+      </label>
+      <input
+        id="data"
+        type='date' 
+        name="data fim"
       />
       <ValidationError 
-        prefix="Message" 
-        field="message"
+     
+        field="email"
+        errors={state.errors}
+      />
+      <label htmlFor="evento">
+        Nome do evento:
+      </label>
+      <input
+        id="evento"
+        type='text' 
+        name="nome do evento"
+      />
+      <ValidationError 
+     
+        field="evento"
+        errors={state.errors}
+      />
+      <label htmlFor="data">
+        Ficha técnica e sinopse:
+      </label>     
+      <textarea
+        id="ficha"
+        name="ficha tecnica e sinopse"
+      />
+      <ValidationError 
+         
+        field="ficha"
+        errors={state.errors}
+      />
+      <label htmlFor="empresa">
+        Empresa/Produtora:
+      </label>
+      <input
+        id="empresa"
+        type='text' 
+        name="empresa"
+      />
+      <ValidationError 
+     
+        field="empresa"
+        errors={state.errors}
+      />
+      <label htmlFor="celular">
+        Celular com DDD:
+      </label>
+      <input
+        id="celular"
+        type='number' 
+        name="celular"
+      />
+      <ValidationError 
+     
+        field="celular"
         errors={state.errors}
       />
       <button type="submit" disabled={state.submitting}>
