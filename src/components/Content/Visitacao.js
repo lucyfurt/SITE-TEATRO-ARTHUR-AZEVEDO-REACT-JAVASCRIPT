f6ce08e65b7d9d7764bf3453bc62d848a60c18bb
lucyfurt <lucianafurtado10@gmail.com>
2023-06-26 14:07:12 -0300

    visit

--- src/components/Content/Visitacao.js
+++ src/components/Content/Visitacao.js
@@ -13,50 +13,47 @@ function Visitacao() {
 
   return (
     <div>
-    <div className="container">
-      <Tabs defaultActiveKey="1" centered>
-        <TabPane
-          tab={
-            <span>
-              <EnvironmentTwoTone />
-              Turística
-            </span>
-          }
-          key="1"
-        >
-          <Card className='visit'>
-            
-            <p><strong>Terça das 14h às 17h</strong></p>
-            <p>
-              Não é necessário agendamento, basta chegar à bilheteria e receber as instruções.
-              Dependendo da quantidade de visitantes, a visita poderá ser dividida em grupos
-              para melhor aproveitamento e segurança de todos.
-            </p>
-          </Card>
-        </TabPane>
-        <TabPane
-          tab={
-            <span>
-              <BankTwoTone />
-              Instituições
-            </span>
-          }
-          key="2"
-        >
-          <Card className='visit'>
-            
-            <p><strong>Terça das 14h às 17h</strong></p>
-            <p>
-              Agendamento por meio de ofício contendo as seguintes informações: dia,
-              quantidade de visitantes e faixa etária, deverá ser enviado para o email:
-            </p>
-            <p className="email"><MessageTwoTone /><strong>pauta.taa@gmail.com</strong></p>
-
-          </Card>
-        </TabPane>
-      </Tabs>
-     </div>
-   </div>
+      <div className="container">
+        <Tabs defaultActiveKey="1" centered>
+          <TabPane
+            tab={
+              <span>
+                <EnvironmentTwoTone />
+                Turística
+              </span>
+            }
+            key="1"
+          >
+            <Card className='visit'>
+              <p className="opening-hours"><strong>Terça das 14h às 17h</strong></p>
+              <p>
+                Não é necessário agendamento, basta chegar à bilheteria e receber as instruções.
+                Dependendo da quantidade de visitantes, a visita poderá ser dividida em grupos
+                para melhor aproveitamento e segurança de todos.
+              </p>
+            </Card>
+          </TabPane>
+          <TabPane
+            tab={
+              <span>
+                <BankTwoTone />
+                Instituições
+              </span>
+            }
+            key="2"
+          >
+            <Card className='visit'>
+              <p className="opening-hours"><strong>Terça das 14h às 17h</strong></p>
+              <p>
+                Agendamento por meio de ofício contendo as seguintes informações: dia,
+                quantidade de visitantes e faixa etária, deverá ser enviado para o email:
+              </p>
+              <p className="email"><MessageTwoTone /><strong>pauta.taa@gmail.com</strong></p>
+            </Card>
+          </TabPane>
+        </Tabs>
+      </div>
+    </div>
   );
 }
 
