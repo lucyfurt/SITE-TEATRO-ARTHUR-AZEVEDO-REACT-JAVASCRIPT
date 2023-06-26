2b86df5381083dc38410b42658d023e8eebe7629
lucyfurt <lucianafurtado10@gmail.com>
2023-06-26 13:30:25 -0300

    visit updated

--- src/components/Content/Visitacao.js
+++ src/components/Content/Visitacao.js
@@ -1,69 +1,70 @@
-import React, { useState } from 'react';
-import { Row, Col, Button, Modal } from 'antd';
+import React from 'react';
+import { Card, Tabs } from 'antd';
 import {
   EnvironmentTwoTone,
   BankTwoTone,
   CheckCircleTwoTone,
   MessageTwoTone
 } from '@ant-design/icons';
-import './Visitacao.css'
+import './Visitacao.css';
+
+const { TabPane } = Tabs;
 
 function Visitacao() {
-  const [size, setSize] = useState('large');
-  const [modal2Open, setModal2Open] = useState(false);
+
 
   return (
-    <>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '50px' }}>
-        <Row gutter={[16, 16]}>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <h1><EnvironmentTwoTone />Turística</h1>
+    <div className="container">
+      <Tabs defaultActiveKey="1" centered>
+        <TabPane
+          tab={
+            <span>
+              <EnvironmentTwoTone />
+              Turística
+            </span>
+          }
+          key="1"
+        >
+          <Card className='visit'>
+            <h1 className="title">Turística</h1>
             <p><strong>Terça das 14h às 17h</strong></p>
             <p>
               Não é necessário agendamento, basta chegar à bilheteria e receber as instruções.
               Dependendo da quantidade de visitantes, a visita poderá ser dividida em grupos
               para melhor aproveitamento e segurança de todos.
             </p>
-          </Col>
-          <Col xs={24} sm={12} md={12} lg={12}>
-            <h1><BankTwoTone />Instituições</h1>
+            <h2>Protocolos</h2>
+            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Proibido fotos em câmera profissional sem uma autorização prévia.</p>
+            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />É permanentemente proibido entrada nas dependências internas do Teatro com alimentos e bebidas.</p>
+            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Não é permitido o tour por menores desacompanhados.</p>
+          </Card>
+        </TabPane>
+        <TabPane
+          tab={
+            <span>
+              <BankTwoTone />
+              Instituições
+            </span>
+          }
+          key="2"
+        >
+          <Card className='visit'>
+            <h1 className="title">Instituições</h1>
             <p><strong>Terça das 14h às 17h</strong></p>
             <p>
               Agendamento por meio de ofício contendo as seguintes informações: dia,
               quantidade de visitantes e faixa etária, deverá ser enviado para o email:
             </p>
-            <p><strong><MessageTwoTone />pauta.taa@gmail.com</strong></p>
-          </Col>
-        </Row>
-      </div>
+            <p className="email"><MessageTwoTone /><strong>pauta.taa@gmail.com</strong></p>
 
-      <Button
-        style={{ backgroundColor: '#f2ae30' }}
-        className="button text-center"
-        type="primary"
-        shape="primary"
-        size={size}
-        onClick={() => setModal2Open(true)}
-      >
-        Mais informações
-      </Button>
-
-      <Modal
-        title="Protocolos"
-        centered
-        visible={modal2Open}
-        onOk={() => setModal2Open(false)}
-        onCancel={() => setModal2Open(false)}
-        style={{ backgroundColor: '#ffcc80' }}
-        className="button text-center"
-        type="primary"
-        shape="primary"
-      >
-        <p><CheckCircleTwoTone />Proibido fotos em câmera profissional sem uma autorização prévia.</p>
-        <p><CheckCircleTwoTone />É permanentemente proibido entrada nas dependências internas do Teatro com alimentos e bebidas.</p>
-        <p><CheckCircleTwoTone />Não é permitido o tour por menores desacompanhados.</p>
-      </Modal>
-    </>
+            <h2>Protocolos</h2>
+            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Proibido fotos em câmera profissional sem uma autorização prévia.</p>
+            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />É permanentemente proibido entrada nas dependências internas do Teatro com alimentos e bebidas.</p>
+            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Não é permitido o tour por menores desacompanhados.</p>
+          </Card>
+        </TabPane>
+      </Tabs>
+    </div>
   );
 }
 
