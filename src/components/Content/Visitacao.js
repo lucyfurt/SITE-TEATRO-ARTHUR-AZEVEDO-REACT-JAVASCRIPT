29c511c5102aa2514bb9dadea9d4e3020d9f00a2
lucyfurt <lucianafurtado10@gmail.com>
2023-06-26 14:00:27 -0300

    visit updated

--- src/components/Content/Visitacao.js
+++ src/components/Content/Visitacao.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Card, Tabs } from 'antd';
+import { Card, Tabs} from 'antd';
 import {
   EnvironmentTwoTone,
   BankTwoTone,
-  CheckCircleTwoTone,
-  MessageTwoTone
+  MessageTwoTone,
 } from '@ant-design/icons';
 import './Visitacao.css';
 
@@ -12,8 +11,8 @@ const { TabPane } = Tabs;
 
 function Visitacao() {
 
-
   return (
+    <div>
     <div className="container">
       <Tabs defaultActiveKey="1" centered>
         <TabPane
@@ -26,17 +25,13 @@ function Visitacao() {
           key="1"
         >
           <Card className='visit'>
-            <h1 className="title">Turística</h1>
+            
             <p><strong>Terça das 14h às 17h</strong></p>
             <p>
               Não é necessário agendamento, basta chegar à bilheteria e receber as instruções.
               Dependendo da quantidade de visitantes, a visita poderá ser dividida em grupos
               para melhor aproveitamento e segurança de todos.
             </p>
-            <h2>Protocolos</h2>
-            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Proibido fotos em câmera profissional sem uma autorização prévia.</p>
-            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />É permanentemente proibido entrada nas dependências internas do Teatro com alimentos e bebidas.</p>
-            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Não é permitido o tour por menores desacompanhados.</p>
           </Card>
         </TabPane>
         <TabPane
@@ -49,7 +44,7 @@ function Visitacao() {
           key="2"
         >
           <Card className='visit'>
-            <h1 className="title">Instituições</h1>
+            
             <p><strong>Terça das 14h às 17h</strong></p>
             <p>
               Agendamento por meio de ofício contendo as seguintes informações: dia,
@@ -57,14 +52,11 @@ function Visitacao() {
             </p>
             <p className="email"><MessageTwoTone /><strong>pauta.taa@gmail.com</strong></p>
 
-            <h2>Protocolos</h2>
-            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Proibido fotos em câmera profissional sem uma autorização prévia.</p>
-            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />É permanentemente proibido entrada nas dependências internas do Teatro com alimentos e bebidas.</p>
-            <p className="protocol"><CheckCircleTwoTone className="protocol-icon" />Não é permitido o tour por menores desacompanhados.</p>
           </Card>
         </TabPane>
       </Tabs>
-    </div>
+     </div>
+   </div>
   );
 }
 
