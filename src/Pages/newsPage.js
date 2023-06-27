c3278b2aa81e86b12a041d60e659e94239f5f639
lucyfurt <lucianafurtado10@gmail.com>
2023-06-27 16:44:34 -0300

    all News updated

--- src/Pages/newsPage.js
+++ src/Pages/newsPage.js
@@ -1,66 +1,73 @@
+import { List } from 'antd';
 import React from 'react';
 import { Layout, Typography } from 'antd';
 
 const { Content, Header, Footer } = Layout;
 const { Title } = Typography;
 
-const newsData = [
+
+const data = [
   {
-    title: 'Notícia 1',
-    content: 'Descrição da notícia 1',
+    title: 'Ciclo de Formação em Artes Cênicas no Teatro Arthur Azevedo: Uma experiência inesquecível!',
+    description: 'O Ciclo de Formação em Artes Cênicas no Teatro Arthur Azevedo foi uma experiência transformadora. Através de workshops intensivos, práticas de cena e um ambiente acolhedor, foram exploradas diferentes técnicas e gêneros teatrais',
     image: 'https://i.ibb.co/y6BjDVk/Whats-App-Image-2023-05-25-at-17-40-32.jpg',
-    author: 'Vitor Silper',
-    link: '/allNews'
-
   },
   {
-    title: 'Notícia 2',
-    content: 'Descrição da notícia 2',
-    author: 'Vitor Silper',
+    title: 'Celebração dos 206 Anos do Teatro Arthur Azevedo: Uma história de arte e cultura!',
+    description: 'Na semana de comemoração ao aniversário de 206 anos do Teatro Arthur Azevedo, uma atmosfera vibrante tomou conta do local. O teatro foi palco de várias experiências emocionantes, encantando o público com visitações, performance teatral, shows. Destacando a importância do Teatro Arthur Azevedo como um ícone da cultura e da arte.',
     image: 'https://i.ibb.co/pJTrCbf/206taa.png',
-    link: '/allNews'
   },
   {
-    title: 'Notícia 3',
-    content: 'Descrição da notícia 3',
-    author: 'Vitor Silper',
+    title: 'Teatro Arthur Azevedo inaugura Núcleo de Memória e Pesquisa: preservando a história e impulsionando a criação artística!',
+    description: 'O Teatro Arthur Azevedo inaugurou seu Núcleo de Memória e Pesquisa, um espaço dedicado à preservação e estudo da história teatral. Com um acervo rico em documentos e registros audiovisuais, o núcleo busca resgatar memórias, estimular pesquisas e inspirar novas criações artísticas. Além disso, promoverá atividades como palestras e exposições, fortalecendo o compromisso do teatro em preservar a cultura e valorizar sua importância como agente transformador na sociedade.',
     image: 'https://i.ibb.co/Zf2MNgH/Whats-App-Image-2023-05-25-at-17-40-23.jpg',
-    link: '/allNews'
   },
-]
+];
 
-const NewsPage = () => {
-  return (
-    <Layout>
-      <Header style={{ background: '#6d0202', padding: '20px' }}>
-        <Title level={3} style={{ color: '#fff', margin: 0 }}>
-          <a style={{ color: '#fff' }} href='/'>Teatro Arthur Azevedo</a>
-        </Title>
-      </Header>
+const App = () => (
+  <Layout>
+    <Header style={{ background: '#6d0202', padding: '20px' }}>
+      <Title level={3} style={{ color: '#fff', margin: 0 }}>
+        <a style={{ color: '#fff' }} href='/'>Teatro Arthur Azevedo</a>
+      </Title>
+    </Header>
+    <Content style={{ padding: '50px' }}>
+      <Title level={2}>Notícias</Title>
 
-      <Content style={{ padding: '50px' }}>
-        <Title level={2}>Notícias</Title>
-        <div className="news-page-container">
-          <div className="news-page">
-            <div className="news-container">
-              {newsData.map((news, index) => (
-                <div key={index} className="news-card">
-                  <img src={news.image} alt={news.title} className="news-image" />
-                  <h2>{news.title}</h2>
-                  <p>{news.content}</p>
-                  <p className="author">Por: {news.author}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-        </div>
-      </Content>
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+        }}
+        dataSource={data}
+        renderItem={(item) => (
 
-      <Footer style={{ background: '#6d0202', padding: '20px', textAlign: 'center', color: '#fff' }}>
-        Teatro Arthur Azevedo  © Todos os direitos reservados 2023
-      </Footer>
-    </Layout>
-  );
-};
+          <List.Item
+            key={item.title}
+            extra={
+              <img
+                width={272}
+                alt="logo"
+                src={item.image}
+              />
+            }
+          >
+            <List.Item.Meta
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
+            />
+          </List.Item>
 
-export default NewsPage;
+        )}
+      />
+    </Content>
+    <Footer style={{ background: '#6d0202', padding: '20px', textAlign: 'center', color: '#fff' }}>
+      Teatro Arthur Azevedo  © Todos os direitos reservados 2023
+    </Footer>
+  </Layout>
+);
+
+export default App;
