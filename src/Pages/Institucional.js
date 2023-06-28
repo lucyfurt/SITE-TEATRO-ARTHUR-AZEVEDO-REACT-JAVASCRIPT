a518207a6bca252e30c34e8bbdc96ca9c810a488
lucyfurt <lucianafurtado10@gmail.com>
2023-06-28 15:49:09 -0300

    change layout institucional

--- src/Pages/Institucional.js
+++ src/Pages/Institucional.js
@@ -1,81 +1,98 @@
 import React from 'react';
-import { Card, Avatar, Layout, Typography, Divider } from 'antd';
+import { Layout, Typography, Tabs, Card, Avatar, Divider } from 'antd';
 import {
-    UserOutlined, TeamOutlined, ScheduleOutlined,
-    DollarCircleOutlined, ToolOutlined, LaptopOutlined, BookOutlined, UserAddOutlined
+  UserOutlined,
+  TeamOutlined,
+  ScheduleOutlined,
+  DollarCircleOutlined,
+  ToolOutlined,
+  LaptopOutlined,
+  BookOutlined,
+  UserAddOutlined,
 } from '@ant-design/icons';
 
 const { Content, Header, Footer } = Layout;
-const { Meta } = Card;
 const { Title } = Typography;
+const { TabPane } = Tabs;
+const { Meta } = Card;
 
 const Institutional = () => {
-    return (
-        <div>
-            <Layout>
-            <Header style={{ background: '#6d0202', padding: '20px' }}>
-                <Title level={3} style={{ color: '#fff', margin: 0 }}>
-                  <a style={{color: '#fff'}}href='/'>Teatro Arthur Azevedo</a>  
-                </Title>
-                </Header>
-                <Content style={{ padding: '20px' }}>
-                    <Card title="Institucional" style={{ width: 500, margin: '0 auto' }}>
-                        <Meta
-                            avatar={<Avatar icon={<UserOutlined />} />}
-                            title="Diretor do Teatro"
-                            description="Victor Silper"
-                        />
-                        <Divider />
-                        <Meta
-                            avatar={<Avatar icon={<TeamOutlined />} />}
-                            title="Diretora Administrativa"
-                            description="Karla Brito"
-                        />
-                        <Divider />
-                        <Meta
-                            avatar={<Avatar icon={<ScheduleOutlined />} />}
-                            title="Chefe de Pauta"
-                            description="Samuel Viegas"
-                        />
-                        <Divider />
-                        <Meta
-                            avatar={<Avatar icon={<ToolOutlined />} />}
-                            title="Coordenação técnico"
-                            description="Abel Lopes"
-                        />
-                        <Divider />
-                        <Meta
-                            avatar={<Avatar icon={<DollarCircleOutlined />} />}
-                            title="Chefe de Bilheteria"
-                            description="Larissa Ferreira"
-                        />
-                        <Divider />
-                        <Meta
-                            avatar={<Avatar icon={<LaptopOutlined />} />}
-                            title="Coordenação de produção"
-                            description="Joás Coelho"
-                        />
-                        <Divider />
-                        <Meta
-                            avatar={<Avatar icon={<BookOutlined />} />}
-                            title="Coordenação do NAE"
-                            description="Ione Coelho"
-                        />
-                        <Divider />
-                        <Meta
-                            avatar={<Avatar icon={<UserAddOutlined />} />}
-                            title="Chefe de serviços gerais"
-                            description="Maria Francisca Moraes"
-                        />
-                        <Divider />
-                    </Card>
-                </Content>
-                <Footer style={{ background: '#6d0202', padding: '20px', textAlign: 'center', color: '#fff' }}>
-                    Teatro Arthur Azevedo  © Todos os direitos reservados 2023
-                </Footer>
-            </Layout>
-        </div>
-    );
+  return (
+    <Layout>
+      <Header style={{ background: '#6d0202', padding: '20px' }}>
+        <Title level={3} style={{ color: '#fff', margin: 0 }}>
+          <a style={{ color: '#fff' }} href='/'>Teatro Arthur Azevedo</a>
+        </Title>
+      </Header>
+      <Content style={{ padding: '50px' }}>
+        <Tabs tabPosition="left">
+          <TabPane tab="Diretores" key="1">
+            <Card title="Diretor do Teatro">
+              <Meta
+                avatar={<Avatar icon={<UserOutlined />} />}
+                title="Victor Silper"
+              />
+            </Card>
+            <Divider />
+            <Card title="Diretora Administrativa">
+              <Meta
+                avatar={<Avatar icon={<TeamOutlined />} />}
+                title="Karla Brito"
+              />
+            </Card>
+          </TabPane>
+          <TabPane tab="Equipe Técnica" key="2">
+            <Card title="Chefe de Pauta">
+              <Meta
+                avatar={<Avatar icon={<ScheduleOutlined />} />}
+                title="Samuel Viegas"
+              />
+            </Card>
+            <Divider />
+            <Card title="Coordenação técnico">
+              <Meta
+                avatar={<Avatar icon={<ToolOutlined />} />}
+                title="Abel Lopes"
+              />
+            </Card>
+          </TabPane>
+          <TabPane tab="Administração" key="3">
+            <Card title="Chefe de Bilheteria">
+              <Meta
+                avatar={<Avatar icon={<DollarCircleOutlined />} />}
+                title="Larissa Ferreira"
+              />
+            </Card>
+            <Divider />
+            <Card title="Coordenação de produção">
+              <Meta
+                avatar={<Avatar icon={<LaptopOutlined />} />}
+                title="Joás Coelho"
+              />
+            </Card>
+          </TabPane>
+          <TabPane tab="Outros" key="4">
+            <Card title="Coordenação do NAE">
+              <Meta
+                avatar={<Avatar icon={<BookOutlined />} />}
+                title="Ione Coelho"
+              />
+            </Card>
+            <Divider />
+            <Card title="Chefe de serviços gerais">
+              <Meta
+                avatar={<Avatar icon={<UserAddOutlined />} />}
+                title="Maria Francisca Moraes"
+              />
+            </Card>
+          </TabPane>
+        </Tabs>
+      </Content>
+      <Footer style={{ background: '#6d0202', padding: '20px', textAlign: 'center', color: '#fff' }}>
+        Teatro Arthur Azevedo © Todos os direitos reservados {new Date().getFullYear()}
+      </Footer>
+    </Layout>
+  );
 };
 
 export default Institutional;
