8f17e6e48d234a1ddbea0dfeaeddf88b3164b06e
lucyfurt <lucianafurtado10@gmail.com>
2023-06-28 13:38:34 -0300

    change photos

--- src/components/Header/Carrossel.js
+++ src/components/Header/Carrossel.js
@@ -8,8 +8,9 @@ function Carrossel() {
     <Carousel autoplay>
       {/* Slide 1 */}
       <div>
+     
         <img
-          src='https://i.ibb.co/XLHVTWB/0005.png'
+          src=' https://i.ibb.co/LCkKg7M/0004.png'
           style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
         />
       </div>
@@ -17,31 +18,34 @@ function Carrossel() {
       {/* Slide 2 */}
       <div>
         <img
-          src='https://i.ibb.co/LCkKg7M/0004.png'
+          src=' https://i.ibb.co/yqz8DqH/0001.png'
           style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
         />
       </div>
       {/* Slide 3 */}
       <div>
         <img
-          src='https://i.ibb.co/wL61HpC/0003.png'
+          src='https://i.ibb.co/KmQXytP/0002.png'
           style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
         />
       </div>
        {/* Slide 4 */}
        <div>
+      
         <img
-          src='https://i.ibb.co/KmQXytP/0002.png'
+          src=' https://i.ibb.co/wL61HpC/0003.png'
           style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
         />
       </div>
        {/* Slide 5 */}
        <div>
+       
         <img
-          src='https://i.ibb.co/yqz8DqH/0001.png'
+          src='https://i.ibb.co/XLHVTWB/0005.png'
           style={{ width: '100%', height: isSmallScreen ? 'auto' : '700px' }}
         />
       </div>
+     
     </Carousel>
   );
 }
