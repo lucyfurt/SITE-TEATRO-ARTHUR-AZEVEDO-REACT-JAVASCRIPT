21ec02e3f1bb8d4080841eedec5b7c85fce40133
lucyfurt <lucianafurtado10@gmail.com>
2023-07-11 17:54:12 -0300

    historia atualizado

--- src/Pages/Historia.js
+++ src/Pages/Historia.js
@@ -1,66 +1,63 @@
 import React from 'react';
-import { Layout, Typography, Tabs, Carousel } from 'antd';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Layout, Typography } from 'antd';
 import './Historia.css'
 
 const { Content, Header, Footer } = Layout;
 const { Title } = Typography;
-const { TabPane } = Tabs;
+
 function Historia() {
-return (
-<Layout>
-<Header style={{ background: '#6d0202', padding: '20px' }}>
-<Title level={3} style={{ color: '#fff', margin: 0 }}>
-<a style={{ color: '#fff' }} href='/'>Teatro Arthur Azevedo</a>
-</Title>
-</Header>
-<Content style={{ padding: '50px'}}>
-<Title level={2}>História</Title>
-<Tabs defaultActiveKey="1" type="card">
-<TabPane tab="Fundação" key="1" style={{ fontSize:'24px' }}>
-<Title level={4}>Fundação</Title>
-<p>No ano de 1815, dois comerciantes portugueses desejavam ter um teatro em São Luís, no Maranhão, que fosse semelhante aos da Europa. Apesar das dificuldades enfrentadas, eles construíram o Teatro União, inaugurado em 1817. O teatro foi posteriormente renomeado para Teatro São Luiz e, mais tarde, recebeu o nome de Teatro Arthur Azevedo em homenagem a um importante teatrólogo maranhense. O local passou por crises, funcionou como cinema e foi restaurado várias vezes. Hoje, é considerado um dos principais teatros históricos do Brasil, sendo palco de diversas apresentações artísticas e eventos culturais. Sua arquitetura neoclássica e sua relevância cultural o tornam um símbolo da arte no Maranhão.</p>
-<Carousel autoplay>
-<div>
-<img src="https://i.ibb.co/8Ppnk0Q/Teatro-Arthur-Azevedo.jpg" alt="Fundação" width={'100%'} />
-</div>
-</Carousel>
-</TabPane>
-<TabPane tab="Apolônia Pinto" key="2" style={{ fontSize:'24px' }}>
-<Title level={4}>Apolônia Pinto</Title>
-<p>Apolônia Pinto foi uma atriz maranhense que se destacou como uma das maiores intérpretes do teatro brasileiro. Nascida em 1854, ela nasceu nos bastidores do Teatro Arthur Azevedo, então chamado de Teatro União e Teatro São Luiz. Desde cedo, demonstrou talento e aos doze anos de idade já encantava plateias com sua atuação na peça "A Cigana de Paris". Ao longo de sua carreira, que durou 83 anos, Apolônia se tornou conhecida como a maior atriz do país, emocionando o público com suas interpretações das paixões humanas. É lembrada como uma artista que trabalhava por amor à arte, em uma época em que os artistas dependiam de doações para sobreviver. Seu legado é reverenciado no camarim nº 1 do Teatro Arthur Azevedo, onde placas e homenagens perpetuam sua memória.</p>
-<Carousel autoplay>
-<div>
-<img src="https://i.ibb.co/JxKBXZC/IMG-7889.jpg" alt="Apolônia Pinto" width={'100%'} />
-</div>
-</Carousel>
-</TabPane>
-<TabPane tab="Arthur Azevedo" key="3" style={{ fontSize:'24px' }}>
-<Title level={4}>Arthur Azevedo</Title>
-<p>Arthur Azevedo foi um renomado comediógrafo brasileiro do século XIX, conhecido por sua habilidade como autor teatral, poeta e crítico. Nascido em São Luís, Maranhão, ele se destacou como um dos principais escritores da belle époque no Rio de Janeiro, retratando a vida cotidiana e as peculiaridades da sociedade da época. Suas peças teatrais, contos e poesias abordavam principalmente o cenário carioca, revelando as fraquezas, alegrias e medos da geração da época. Além disso, Azevedo foi um defensor da vida e promoveu uma campanha contra o suicídio. Embora algumas de suas obras originais tenham se perdido, seu legado perdura na história do teatro e da literatura brasileira.</p>
-<Carousel autoplay>
-<div>
-<img src="https://i.ibb.co/yfP12Y1/IMG-7888.jpg" alt="Arthur Azevedo" width={'100%'} />
-</div>
-</Carousel>
-</TabPane>
-<TabPane tab="Atualidades" key="4" style={{ fontSize:'24px' }}>
-<Title level={4}>Atualidades</Title>
-<p>O trabalho de Arthur Azevedo foi marcado pelo estímulo ao surgimento de novos talentos e pela colaboração com outros comediógrafos, como Moreira Sampaio. Ele valorizava a solidariedade humana e dedicou-se a ajudar artistas envelhecidos e suas famílias, alimentando crianças órfãs e enteados. Embora tenham sido preservados apenas alguns de seus manuscritos, seu impacto na cultura brasileira é inegável, deixando um legado duradouro na história do teatro e na produção literária do país.</p>
-<Carousel autoplay>
-<div>
-<img src="https://i.ibb.co/LCkKg7M/0004.png" alt="Atualidade" width={'100%'} />
-</div>
-</Carousel>
-</TabPane>
-</Tabs>
-</Content>
-<Footer style={{ background: '#6d0202', padding: '20px', textAlign: 'center', color: '#fff' }}>
-<Title level={5} style={{ color: '#fff', margin: 0 }}>
-Teatro Arthur Azevedo © Todos os direitos reservados {new Date().getFullYear()}
-</Title>
-</Footer>
-</Layout>
-);
+    return (
+        <Layout>
+            <Header style={{ background: '#6d0202', padding: '20px' }}>
+                <Title level={3} style={{ color: '#fff', margin: 0 }}>
+                    <a style={{ color: '#fff' }} href='/'>Teatro Arthur Azevedo</a>
+                </Title>
+            </Header>
+            <Content style={{ padding: '50px' }}>
+                <h1>História</h1>
+
+                <div style={{ display: 'block', width: 700, padding: 30 }}>
+
+                    <Carousel >
+                    <Carousel.Item >
+                            <img
+                                className="d-block w-100"
+                                src="https://i.ibb.co/8Ppnk0Q/Teatro-Arthur-Azevedo.jpg"
+                                alt="Image Two"
+                            />
+                            <Carousel.Caption>
+                                <h3>Fundação</h3>
+                                <p>No ano de 1815, dois comerciantes portugueses desejavam ter um teatro em São Luís, no Maranhão, que fosse semelhante aos da Europa. Apesar das dificuldades enfrentadas, eles construíram o Teatro União, inaugurado em 1817. O teatro foi posteriormente renomeado para Teatro São Luiz e, mais tarde, recebeu o nome de Teatro Arthur Azevedo em homenagem a um importante teatrólogo maranhense. O local passou por crises, funcionou como cinema e foi restaurado várias vezes. Hoje, é considerado um dos principais teatros históricos do Brasil, sendo palco de diversas apresentações artísticas e eventos culturais. Sua arquitetura neoclássica e sua relevância cultural o tornam um símbolo da arte no Maranhão.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item >
+                            <img
+                                className="d-block w-100"
+                                src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                                alt="Image One"
+                            />
+                            <Carousel.Caption>
+                                <h3>Label for first slide</h3>
+                                <p>Sample Text for Image One</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        
+                    </Carousel>
+                </div>
+                <h1>TAA 206 Anos</h1>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/-lxloupKM4I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </Content>
+            <Footer style={{ background: '#6d0202', padding: '20px', textAlign: 'center', color: '#fff' }}>
+                <Title level={5} style={{ color: '#fff', margin: 0 }}>
+                    Teatro Arthur Azevedo © Todos os direitos reservados {new Date().getFullYear()}
+                </Title>
+            </Footer>
+        </Layout>
+
+
+    );
 }
 
 export default Historia;
