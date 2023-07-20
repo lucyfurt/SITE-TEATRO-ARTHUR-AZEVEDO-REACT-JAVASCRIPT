7bd89b7354541e2df0fb980753babf7ff6a42487
lucyfurt <lucianafurtado10@gmail.com>
2023-07-20 14:49:47 -0300

    iframe fix

--- src/components/Content/Bilheteria.js
+++ src/components/Content/Bilheteria.js
@@ -15,7 +15,7 @@ function Bilheteria() {
                 <h2>Como chegar</h2>
                 <iframe
                     title="Mapa da bilheteria"
-                    width="1116"
+                    width="700"
                     height="500"
                     frameBorder="0"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.9322008938943!2d-44.3050561259277!3d-2.529032438245369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7f68ee5fb3ba277%3A0x3f66c06359ed1621!2sTeatro%20Arthur%20Azevedo!5e0!3m2!1spt-BR!2sbr!4v1684798082286!5m2!1spt-BR!2sbr"
