87e36835a17a715fb336b096e3b4dda4079bcec3
lucyfurt <lucianafurtado10@gmail.com>
2023-07-25 16:26:13 -0300

    puatas

--- src/Pages/Pautas.js
+++ src/Pages/Pautas.js
@@ -6,47 +6,53 @@ import '../Pages/css/pauta.css';
 const { Content } = Layout;
 
 function Historia() {
-    return (
-        <>
-            <MenuHome />
-            <Layout>
-                <Divider orientation="left">
-                    <h1>Pautas</h1>
-                </Divider>
-                <Content>
-                    <Row>
-                        <Col>
-                            <button className="big-rectangle-button">
-                                Solicitação de pauta Espetáculos/Shows
-                            </button>
-                        </Col>
-                        <Col>
-                            <button className="big-rectangle-button">
-                                Solicitação de pauta Fotografias
-                            </button>
-                        </Col>
-                        <Col>
-                            <button className="big-rectangle-button">
-                                Informaçõoes Técnicas
-                            </button>
+  return (
+    <>
+      <MenuHome />
+      <Layout>
+        <Divider orientation="left">
+          <h1>Pautas</h1>
+        </Divider>
+        <Content>
+          <div className="image-container">
+            <img
+              src="https://i.ibb.co/gDx0FSh/ballerinas-1376250-1280.jpg"
+              width={'400px'}
+              alt="Ballerinas"
+            />
+          </div>
+          <div className="buttons-container">
+            <Row gutter={[16, 16]}>
+              <Col>
+                <button className="big-rectangle-button">
+                  Solicitação de pauta Espetáculos/Shows
+                </button>
+              </Col>
+              <Col>
+                <button className="big-rectangle-button">
+                  Solicitação de pauta Fotografias
+                </button>
+              </Col>
+            </Row>
 
-                        </Col>
-                        <Col>
-
-                            <button className="big-rectangle-button">
-                                Edital/Normas
-                            </button>
-                        </Col>
-                    </Row>
-
-
-
-
-
-                </Content>
-                <FooterComponent />
-            </Layout>
-        </>
-    );
+            <Row gutter={[16, 16]}>
+              <Col>
+                <button className="big-rectangle-button">
+                  Informações Técnicas
+                </button>
+              </Col>
+              <Col>
+                <button className="big-rectangle-button">
+                  Edital/Normas
+                </button>
+              </Col>
+            </Row>
+          </div>
+        </Content>
+        <FooterComponent />
+      </Layout>
+    </>
+  );
 }
+
 export default Historia;
