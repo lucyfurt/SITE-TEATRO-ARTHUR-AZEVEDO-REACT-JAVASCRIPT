1faa19680e9eedb377006743ff362bd6842ada88
lucyfurt <lucianafurtado10@gmail.com>
2023-07-26 14:52:36 -0300

    adjust map of bilheteria

--- src/components/Content/Bilheteria.js
+++ src/components/Content/Bilheteria.js
@@ -1,28 +1,35 @@
 import React from 'react';
 import { Image } from 'antd';
-import './css/bilheteria.css'
+import './css/bilheteria.css';
 
 function Bilheteria() {
-    return (
-        <>
-            <div className='container'>
-                <h2>Horário de funcionamento</h2>
-                <p>
-                    <strong>Terça a domingo das 13h30 às 18h30</strong>
-                </p>
-                <h2>Mapa de assentos</h2>
-                <Image src="https://i.ibb.co/f2ysCHK/teatroMP.png" alt="assentos" />
-                <h2>Como chegar</h2>
-                <iframe
-                    title="Mapa da bilheteria"
-                    width="700"
-                    height="500"
-                    frameBorder="0"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.9322008938943!2d-44.3050561259277!3d-2.529032438245369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7f68ee5fb3ba277%3A0x3f66c06359ed1621!2sTeatro%20Arthur%20Azevedo!5e0!3m2!1spt-BR!2sbr!4v1684798082286!5m2!1spt-BR!2sbr"
-                    allowFullScreen
-                ></iframe>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className='container'>
+        <h2>Horário de funcionamento</h2>
+        <p>
+          <strong>Terça a domingo das 13h30 às 18h30</strong>
+        </p>
+        <h2>Mapa de assentos</h2>
+        <Image
+          src="https://i.ibb.co/f2ysCHK/teatroMP.png"
+          alt="assentos"
+          preview={false} // Disable image preview on click for better mobile experience
+        />
+        <h2>Como chegar</h2>
+        <div className="map-container">
+          <iframe
+            title="Mapa da bilheteria"
+            width="100%"
+            height="300"
+            frameBorder="0"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3985.9322008938943!2d-44.3050561259277!3d-2.529032438245369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7f68ee5fb3ba277%3A0x3f66c06359ed1621!2sTeatro%20Arthur%20Azevedo!5e0!3m2!1spt-BR!2sbr!4v1684798082286!5m2!1spt-BR!2sbr"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default Bilheteria;
--- src/components/Content/HistoriaHome.js
+++ src/components/Content/HistoriaHome.js
@@ -5,7 +5,7 @@ import '../Content/css/historia.css';
 function HistoriaHome() {
     return (
         <div className='history'>
-            <iframe width="800" height="500" src="https://www.youtube.com/embed/-lxloupKM4I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe  src="https://www.youtube.com/embed/-lxloupKM4I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     );
 }
