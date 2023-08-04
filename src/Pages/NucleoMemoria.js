a078a195e752b415655f4697181d9732344df113
lucyfurt <lucianafurtado10@gmail.com>
2023-08-04 14:54:08 -0300

    nmp em contrucao

--- src/Pages/NucleoMemoria.js
+++ src/Pages/NucleoMemoria.js
@@ -1,43 +1,22 @@
 import React from 'react';
-import { Card, Row, Col, Typography, Layout, Divider } from 'antd';
+import { Layout, Divider } from 'antd';
 import MenuBurguer from '../components/Header/MenuBurguer.js';
-import FooterComponent from '../components/Footer/FooterComponent';
 const { Content } = Layout;
-const { Title, Text } = Typography;
+
 const NucleoMemoria = () => {
     return (
         <Layout>
             <MenuBurguer />
             <Divider orientation="left">
-                <h1>Núcleo de Memória e Pesquisa</h1>
+                <p>Núcleo de Memória e Pesquisa</p>
             </Divider>
             <Content>
                 <div>
-                    <Row gutter={[16, 16]}>
-                        <Col span={12}>
-                            <Card>
-                                <Title level={2}>Arquivo TAA</Title>
-
-                            </Card>
-                        </Col>
-
-                        <Col span={12}>
-                            <Card>
-                                <Title level={2}>Biblioteca Arão Paranaguá</Title>
-
-                            </Card>
-                        </Col>
-                    </Row>
-                    <Title level={2}>Acervo</Title>
-
-                    <Card>
-                        <Text>
-                            Em construção...
-                        </Text>
-                    </Card>
+                    <h1>
+                        Em construção...
+                    </h1>
                 </div>
             </Content>
-            <FooterComponent />
         </Layout>
     );
 }
